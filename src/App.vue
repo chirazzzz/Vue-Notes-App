2753b5411a7e25f8047ff6894c89c2fd6c46c325
chirazzzz <chiragmehta.uk@gmail.com>
2024-03-20 17:21:26 +0000

    added key to each note (best practice)

--- src/App.vue
+++ src/App.vue
@@ -45,6 +45,7 @@ const addNote = () => {
       <div class="cards-container">
         <div
           v-for="note in notes"
+          :key="note.id"
           class="card"
           :style="{ background: note.backgroundColor }"
         >
